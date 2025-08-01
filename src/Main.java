7f047dad7cc6580c82b3d5fe268d13cf66502bcd
Angel Dadole <adadole17580@liceo.edu.ph>
2025-08-01 09:58:07 +0800

    Activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test Branch!");
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Enter a number" + ":");
+            int number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println();
+        System.out.println("Total Number: " + totalNumber);
+
     }
 }
