bd71a595ad695f4954fdfbdb450d3ae071f92ae3
Angel Dadole <adadole17580@liceo.edu.ph>
2025-09-24 03:46:00 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== Library Management System ===");
+
         Media book1 = new Book("Java Fundamentals", "B001", "Robert Martin");
         Media dvd1 = new DVD("OOP Concepts", "D001", "Tech Films");
         Media mag1 = new Magazine("Programming Weekly", "M001", 15);
